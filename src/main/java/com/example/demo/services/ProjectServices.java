f08578f2c92bc49f3f8f655e80cd16ed6abde080
dolapo47 <adeleyeadedolapomayowa@gmail.com>
2020-02-28 18:21:51 +0100

    implement get all projects and delete projects

--- src/main/java/com/example/demo/services/ProjectServices.java
+++ src/main/java/com/example/demo/services/ProjectServices.java
@@ -1,5 +1,6 @@
 package com.example.demo.services;
 
+import com.example.demo.exceptions.ProjectIdException;
 import com.example.demo.model.Project;
 import com.example.demo.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +13,36 @@ public class ProjectServices {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
-        return projectRepository.save(project);
+        try{
+            project.setProjectIdentifier((project.getProjectIdentifier().toUpperCase()));
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project id '"+project
+                    .getProjectIdentifier().toUpperCase()+"' already exists");
+        }
+
+    }
+
+    public Project findProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId);
+        if(project == null){
+            throw new ProjectIdException("Project id '"+projectId+"' does not exist");
+        }
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects(){
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId);
+
+        if(project == null){
+            throw new ProjectIdException("Cannot delete project with id '"+projectId+". This project does not exist");
+        }
+
+        projectRepository.delete(project);
     }
 
 }
--- src/main/java/com/example/demo/web/ProjectController.java
+++ src/main/java/com/example/demo/web/ProjectController.java
@@ -7,16 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("api/project")
@@ -35,6 +28,23 @@ public class ProjectController {
 
         Project project1 = projectServices.saveOrUpdateProject(project);
         return new ResponseEntity<>(project1, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{projectId}")
+    public ResponseEntity<?>getProjectById(@PathVariable String projectId){
+        Project project = projectServices.findProjectByIdentifier(projectId.toUpperCase());
+        return new ResponseEntity<>(project, HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public Iterable<Project> getAllProjects(){
+        return projectServices.findAllProjects();
+    }
+
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<?> deleteProject(@PathVariable String projectId){
+        projectServices.deleteProjectByIdentifier(projectId);
+        return new ResponseEntity<String>("Project with Id: '"+projectId+"' was deleted", HttpStatus.OK);
     }
 
 }
